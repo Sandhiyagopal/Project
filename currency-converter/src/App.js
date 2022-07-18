7ecee040289ec113e418152e15cd6d803361c344
Sandhiya Gopal <88146172+Sandhiyagopal@users.noreply.github.com>
2022-07-18 14:48:38 +0530

    add

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -2,60 +2,68 @@ import React from "react";
 import {useState} from 'react'
 import axios from 'axios'
 function App() {
-
   let [amount,setAmount]=useState(0)
-  let [from,setFrom]=useState()
-  let [to,setTo]=useState()
+  let [value,setValue]=useState()
+  let [value1,setValue1]=useState()
   let [result,setResult]=useState()
-  
-  let changeHandler=(event)=>{
-    setAmount(parseInt(event.target.value))
+
+  let getamountHandler=(e)=>{
+    setAmount(e.target.value)
   }
-  let fromHandler=(event)=>{
-    setFrom(event.target.value)
+  let fromHandler=(e)=>{
+    setValue(e.target.value)
   }
-  let toHandler=(event)=>{
-    setTo(event.target.value)
-  }
-  let convertHandler=(event)=>{
-    if(from===to){
-      setResult(amount)
-    }
-    axios.get(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`)
-         .then((response)=>{
-         console.log(response.data)
-         console.log(response.data.rates[`${to}`])
-         setResult(response.data.rates[`${to}`])
-         })
+  let toHandler=(e)=>{
+    setValue1(e.target.value)
   }
   let submitHandler=(e)=>{
-    e.preventDefault(); 
+    e.preventDefault()
   }
-
-  return (
-    <div className="container mt-5">
+  let convertHandler=()=>{
+    if(value===value1){
+      setResult(amount)
+    }
+    axios
+    .get(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${value1}&to=${value}`
+    )
+    .then((response)=>{
+      console.log(response.data)
+      console.log(response.data.rates[value])
+      setResult(response.data.rates[value]);
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }  
+  return(
+    <div className="container">
+      <pre>{amount}</pre>
+      <pre>{value}</pre>
+      <pre>{value1}</pre>
+      <pre>{result}</pre>
       <div className="row">
         <div className="col-md-2"></div>
-        <div className="col-md-8">
-          <div className="card">
-            <div className="card-header d-flex bg-success">
-              <h2 className="mx-auto">Currency Converter</h2>
-            </div>
-            <div className="card-body bg-dark text-white">
-              <form onSubmit={submitHandler}>
-              <div className="d-flex">
-                <label className="mx-auto "><h3 >Enter The Amount</h3></label>
-                </div>
-                <div className="form-group">
-                <input className="form-control" type="text" placeholder="Enter the amount"
-                onChange={changeHandler}/>
+          <div className="col-md-7 mt-5">
+            <div className="card">
+              <div className="card-header bg-success d-flex">
+                <h2 className="mx-auto">Currency Converter</h2>
               </div>
-              <div>
-              <label><h3>From:</h3></label>
-              </div>
-              <div className="form-group">
-              <select className="form-control" onChange={fromHandler}>
-              <option>Select the currency </option>
+              <div className="card-body bg-dark text-white">
+                <form onSubmit={submitHandler}>
+                  <div className="d-flex">
+                  <label className="mx-auto"><h3>Enter the amount</h3></label>
+                  </div>
+                  <div className="form-group">
+                    <input className="form-control mx-auto" placeholder="Enter amount to convert"
+                    type="number" onChange={getamountHandler}/>
+                  </div>
+                  <div >
+                    <label ><h3>From:</h3></label>
+                  </div>
+                  <div className="form-group">
+                    <select className="form-control" onChange={fromHandler}>
+                    <option value="-1">Select the currency </option>
                     <option value="INR">INR</option>
                     <option value="USD">USD</option>
                     <option value="AUD">AUD</option>
@@ -72,15 +80,14 @@ function App() {
                     <option value="SGD">SGD</option>
                     <option value="TRY">TRY</option>
                     <option value="ZAR">ZAR</option>
-                </select>
-              </div>
-              <div>
-              <label>
-                <h3>To:</h3></label>
-              </div>
-              <div className="form-group">
-              <select className="form-control" onChange={toHandler}>
-              <option>Select the currency </option>
+                    </select>
+                  </div>
+                  <div>
+                    <label><h3>To:</h3></label>
+                  </div>
+                  <div className="form-group">
+                    <select className="form-control" onChange={toHandler}>
+                    <option value="-1">Select the currency </option>
                     <option value="INR">INR</option>
                     <option value="USD">USD</option>
                     <option value="AUD">AUD</option>
@@ -97,24 +104,27 @@ function App() {
                     <option value="SGD">SGD</option>
                     <option value="TRY">TRY</option>
                     <option value="ZAR">ZAR</option>
-                </select>
+                    </select>
+                  </div>
+                  <br></br>
+                  <div className="d-flex form-group">
+                    <input type="submit" className="btn btn-success mx-auto form-control" value="convert"
+                   onClick={convertHandler}/>
+                  </div>
+                </form>
+                <h5>
+                  { result>=0
+                  ? `The amount of entered in ${value} is  ${result}  (${value1})`
+                :`Please enter amount... Select currency and convert`}
+                </h5>
               </div>
-              <div>
-               <button className="btn btn-success" onClick={convertHandler}>Convert</button>
+              <div className="card-footer bg-success">
+                <h6>...</h6>
               </div>
-              <div>
-              <h4>The converted amount is:  {result} ({to})</h4>
-              </div>
-              </form>
-            </div>
-            <div className="card-footer bg-success">
-              <h6>...</h6>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+  )
 }
-
-export default App;
+export default App
